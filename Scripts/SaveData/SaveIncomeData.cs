e7e8d9f395eecbd05a0bceddbe02a5b2c4ac6589
Matthew Samun <19292426+MSamun@users.noreply.github.com>
2020-01-17 16:51:41 -0500

    Upload Data file definitions via JSON

---
+++ Scripts/SaveData/SaveIncomeData.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace DopeEmpire
+{
+    [Serializable]
+    public class SaveIncomeData
+    {
+        public TotalIncomeData totalIncomeData;
+        public int numberOfOwnedProducts;
+        public int totalNumberOfProductsCanSell;
+        public ProductIncomeData[] productIncomeData;
+
+        public SaveIncomeData()
+        {
+        }
+    }
+
+    #region Income Information
+
+    //NOTE: JSON, for some reason, does not take in properties or private variables. It only takes public variables.
+    [Serializable]
+    public class TotalIncomeData
+    {
+        public float totalDirtyMoney;
+        public float totalCleanMoney;
+        public float totalReputation;
+
+        public float weeklyDirtyMoney;
+        public float weeklyCleanMoney;
+        public float weeklyReputation;
+
+        public TotalIncomeData(float totalDM, float totalCM, float totalREP, float weeklyDM, float weeklyCM, float weeklyREP)
+        {
+            totalDirtyMoney = totalDM;
+            totalCleanMoney = totalCM;
+            totalReputation = totalREP;
+
+            weeklyDirtyMoney = weeklyDM;
+            weeklyCleanMoney = weeklyCM;
+            weeklyReputation = weeklyREP;
+        }
+    }
+
+    [Serializable]
+    public class ProductIncomeData
+    {
+        public int productID;
+        public int level;
+        public float dirtyMoney;
+        public float cleanMoney;
+        public float reputation;
+        public float timer;
+
+        public ProductIncomeData(int productID, int level, float dirtyMoney, float cleanMoney, float reputation, float timer)
+        {
+            this.productID = productID;
+            this.level = level;
+            this.dirtyMoney = dirtyMoney;
+            this.cleanMoney = cleanMoney;
+            this.reputation = reputation;
+            this.timer = timer;
+        }
+    }
+
+    #endregion Income Information
+}
---
+++ Scripts/SaveData/SavePlayerData.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DopeEmpire
+{
+    [Serializable]
+    public class SavePlayerData
+    {
+        public PlayerLevelData playerLevelData;
+        public CharacterData characterData;
+        public CharacterSkillsData[] characterSkillsData;
+
+        public SavePlayerData()
+        {
+        }
+    }
+
+    #region Character/Player Level
+
+    //NOTE: JSON, for some reason, does not take in properties or private variables. It only takes public variables.
+    [Serializable]
+    public class PlayerLevelData
+    {
+        public int currentLevel;
+        public float experienceEarned;
+        public int requiredExperienceToNextLevel;
+
+        public PlayerLevelData(int currentLevel, float experienceEarned, int requiredExperienceToNextLevel)
+        {
+            this.currentLevel = currentLevel;
+            this.experienceEarned = experienceEarned;
+            this.requiredExperienceToNextLevel = requiredExperienceToNextLevel;
+        }
+    }
+
+    [Serializable]
+    public class CharacterData
+    {
+        public int ownedSkillPoints;
+        public int charIconID;
+
+        public CharacterData(int ownedSkillPoints, int charIconID)
+        {
+            this.ownedSkillPoints = ownedSkillPoints;
+            this.charIconID = charIconID;
+        }
+    }
+
+    [Serializable]
+    public class CharacterSkillsData
+    {
+        public int skillID;
+        public int currentSkillLevel;
+
+        public CharacterSkillsData(int skillID, int currentSkillLevel)
+        {
+            this.skillID = skillID;
+            this.currentSkillLevel = currentSkillLevel;
+        }
+    }
+
+    #endregion Character/Player Level
+}
